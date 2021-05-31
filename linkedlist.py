c82e4cbc36a384b33a21cb2111f45f1112d2a52b
Faithfulness Alamu <vaguemail369@gmail.com>
2021-05-31 12:10:13 +0100

    feat: add method to get node at index

--- linkedlist.py
+++ linkedlist.py
@@ -4,7 +4,7 @@ class Node:
 
 
 class LinkedList:
-    head = None
+    head = Node()
     sz = 0
 
     def size(self):
@@ -12,3 +12,15 @@ class LinkedList:
 
     def empty(self):
         return self.head == None
+
+    def value_at(self, index):
+        "Returns the value at index"
+        if self.empty:
+            raise IndexError("list is empty")
+        node = self.head
+        for _ in range(index):
+            if node.next:
+                node = node.next
+            else:
+                raise IndexError("index out of bounds")
+        return node
